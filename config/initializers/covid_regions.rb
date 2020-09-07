d15f5495d9aa4d4345052b1e8a783072ac0405ea
E. Lynette Rayle <elr37@cornell.edu>
2020-09-07 16:53:59 -0400

    tweak styles for mobile

--- config/initializers/covid_regions.rb
+++ config/initializers/covid_regions.rb
@@ -2,9 +2,10 @@
 
 CovidTracker::RegionRegistry.register_usa(state: 'New York', county: 'Cortland')
 CovidTracker::RegionRegistry.register_usa(state: 'New York', county: 'Tompkins')
-CovidTracker::RegionRegistry.register_usa(state: 'New York', county: 'Broome')
-CovidTracker::RegionRegistry.register_usa(state: 'New York', county: 'Onondaga')
-CovidTracker::RegionRegistry.register_usa(state: 'New York', county: 'Cayuga')
-CovidTracker::RegionRegistry.register_usa(state: 'New York', county: 'Madison')
-CovidTracker::RegionRegistry.register_usa(state: 'New York', county: 'Chenango')
-CovidTracker::RegionRegistry.register_usa(state: 'New York', county: 'Tioga')
+
+# CovidTracker::RegionRegistry.register_usa(state: 'New York', county: 'Broome')
+# CovidTracker::RegionRegistry.register_usa(state: 'New York', county: 'Onondaga')
+# CovidTracker::RegionRegistry.register_usa(state: 'New York', county: 'Cayuga')
+# CovidTracker::RegionRegistry.register_usa(state: 'New York', county: 'Madison')
+# CovidTracker::RegionRegistry.register_usa(state: 'New York', county: 'Chenango')
+# CovidTracker::RegionRegistry.register_usa(state: 'New York', county: 'Tioga')
